ca3044c4ab58562711e4da5aea397b02e0ecc002
skid0u <felix.boivin@hotmail.com>
2024-01-15 14:54:33 -0500

    FINAL pour valid

--- pb_APP_log_comb.sim/sim_1/behav/xsim/xsim.dir/Thermo2Bin_TB_behav/webtalk/xsim_webtalk.tcl
+++ pb_APP_log_comb.sim/sim_1/behav/xsim/xsim.dir/Thermo2Bin_TB_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/GitHub/S4_APP1/pb_APP_log_comb.sim/sim_1/behav/xsim/xsim.dir/Thermo2Bin_TB_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sat Jan 13 22:12:01 2024" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Jan 15 14:50:16 2024" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "6e88f38826214958bd01804f64fad5de" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "10" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "13" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 5 3600 6-Core Processor              " -context "user_environment"
@@ -24,9 +24,9 @@ webtalk_add_data -client project -key system_ram -value "34.000 GB" -context "us
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "2200 ns" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "11 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "2" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6536_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2707998493 -regid "" -xml D:/GitHub/S4_APP1/pb_APP_log_comb.sim/sim_1/behav/xsim/xsim.dir/Thermo2Bin_TB_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/GitHub/S4_APP1/pb_APP_log_comb.sim/sim_1/behav/xsim/xsim.dir/Thermo2Bin_TB_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/GitHub/S4_APP1/pb_APP_log_comb.sim/sim_1/behav/xsim/xsim.dir/Thermo2Bin_TB_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "7108_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3868427736 -regid "" -xml D:/GitHub/S4_APP1/pb_APP_log_comb.sim/sim_1/behav/xsim/xsim.dir/Thermo2Bin_TB_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/GitHub/S4_APP1/pb_APP_log_comb.sim/sim_1/behav/xsim/xsim.dir/Thermo2Bin_TB_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/GitHub/S4_APP1/pb_APP_log_comb.sim/sim_1/behav/xsim/xsim.dir/Thermo2Bin_TB_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
